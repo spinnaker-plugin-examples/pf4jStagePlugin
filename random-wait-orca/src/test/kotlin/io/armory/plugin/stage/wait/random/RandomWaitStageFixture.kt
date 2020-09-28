ab46fca8cf4de514d133ab0ab7c8663d83cf0363
Daniel Peach <daniel.peach@armory.io>
2020-09-28 15:43:06 -0400

    remove simple stage (#24)

--- random-wait-orca/src/test/kotlin/io/armory/plugin/stage/wait/random/RandomWaitStageFixture.kt
+++ random-wait-orca/src/test/kotlin/io/armory/plugin/stage/wait/random/RandomWaitStageFixture.kt
@@ -30,34 +30,11 @@ import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 
 @TestPropertySource(properties = [
-  "spinnaker.extensibility.plugins.Armory.RandomWaitEnabledPlugin.enabled=true",
-  "spinnaker.extensibility.plugins.Armory.RandomWaitDisabledPlugin.disabled=true",
-  "spinnaker.extensibility.plugins.Armory.RandomWaitNotSupportedPlugin.enabled=true",
+  "spinnaker.extensibility.plugins.Armory.RandomWaitPlugin.enabled=true",
   "spinnaker.extensibility.plugins-root-path=build/plugins"
 ])
 @AutoConfigureMockMvc
-class OrcaPluginsFixture : PluginsTckFixture, OrcaFixture() {
-
-  final override val plugins = File("build/plugins")
-  final override val enabledPlugin: PluginJar
-  final override val disabledPlugin: PluginJar
-  final override val versionNotSupportedPlugin: PluginJar
-
-  override val extensionClassNames: MutableList<String> = mutableListOf(
-      RandomWaitStage::class.java.name
-  )
-
-  final override fun buildPlugin(pluginId: String, systemVersionRequirement: String): PluginJar {
-    return PluginJar.Builder(plugins.toPath().resolve("$pluginId.jar"), pluginId)
-        .pluginClass(RandomWaitPlugin::class.java.name)
-        .pluginVersion("1.0.0")
-        .manifestAttribute("Plugin-Requires", "orca$systemVersionRequirement")
-        .extensions(extensionClassNames)
-        .build()
-  }
-
-  @Autowired
-  override lateinit var spinnakerPluginManager: SpinnakerPluginManager
+class OrcaPluginsFixture : OrcaFixture() {
 
   @Autowired
   lateinit var stageResolver: StageResolver
@@ -68,10 +45,17 @@ class OrcaPluginsFixture : PluginsTckFixture, OrcaFixture() {
   val mapper = jacksonObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
 
   init {
-    plugins.delete()
-    plugins.mkdir()
-    enabledPlugin = buildPlugin("Armory.RandomWaitEnabledPlugin", ">=1.0.0")
-    disabledPlugin = buildPlugin("Armory.RandomWaitDisabledPlugin", ">=1.0.0")
-    versionNotSupportedPlugin = buildPlugin("Armory.RandomWaitNotSupportedPlugin", ">=1000.0.0")
+    val pluginId = "Armory.RandomWaitPlugin"
+    val plugins = File("build/plugins").also {
+      it.delete()
+      it.mkdir()
+    }
+
+    PluginJar.Builder(plugins.toPath().resolve("$pluginId.jar"), pluginId)
+      .pluginClass(RandomWaitPlugin::class.java.name)
+      .pluginVersion("1.0.0")
+      .manifestAttribute("Plugin-Requires", "orca>=0.0.0")
+      .extensions(mutableListOf(RandomWaitStage::class.java.name, RandomWaitStage.RandomWaitTask::class.java.name))
+      .build()
   }
 }
