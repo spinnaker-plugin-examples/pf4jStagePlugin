697c58381d5ac94e271faaf6b4ee7caa35c7ba5e
Daniel Peach <daniel.peach@armory.io>
2020-10-05 13:08:53 -0400

    update plugin configuration for v2 framework (#26)

--- random-wait-orca/src/main/kotlin/io/armory/plugin/stage/wait/random/RandomWaitConfig.kt
+++ random-wait-orca/src/main/kotlin/io/armory/plugin/stage/wait/random/RandomWaitConfig.kt
@@ -5,7 +5,6 @@ import com.netflix.spinnaker.kork.plugins.api.PluginConfiguration
 /**
  * Data in this class maps to the plugin configuration in a service's config YAML.
  * The data can be key/value pairs or an entire configuration tree.
- *
  */
-@PluginConfiguration("armory.randomWaitStage")
+@PluginConfiguration
 data class RandomWaitConfig(var defaultMaxWaitTime: Int)
